e6799e51701eb064bab9e2d80506ac0287cfd493
Nicholas MacDonald <macdonaldn@ufl.edu>
2022-11-15 20:03:17 -0500

    remove team select/put in daily game summary

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,11 @@
 let nbaEl = document.querySelector("#nba");
 let teamSelect = document.querySelector(".teamSelect");
+let games = document.querySelector("#content")
+let main = document.querySelector(".main__wrapper")
+let LocalDate = new Date()
+let Year = LocalDate.getFullYear()
+let Month = LocalDate.getMonth() + 1
+let Day = LocalDate.getDate()
 
 const options = {
 	method: "GET",
@@ -9,23 +15,31 @@ const options = {
 	}
 };
 
-fetch("https://api-nba-v1.p.rapidapi.com/teams", options)
-	.then((response) => response.json())
-	.then((team) => {
-		console.log(team.response);
-		team.response.map((b) => {
-			if (b.nbaFranchise) {
-				let option = document.createElement("option");
-				option.value = b.id;
-				option.textContent = b.name;
-				teamSelect.appendChild(option);
-			}
-		});
-	})
-	.catch((err) => console.error(err));
+fetch(`https://api-nba-v1.p.rapidapi.com/games?date=${Year}-${Month}-${Day}`, options)
+.then((response) => response.json())
+.then((game) => {
+	console.log(`https://api-nba-v1.p.rapidapi.com/games?date=${Year}-${Month}-${Day}`)
+	console.log(game.response);
+	let data = game.response;
+	for (let i = 0; i < data.length; i++) {
+		games.innerHTML = 
+		`<div style="display: grid; grid-template-columns:25% 20% 40% 20%; grid-template-rows:50% 50%;">
+		<h3>${Month}/${Day}</h3>
+		<img src="${data[i].teams.visitors.logo}" style="height: 100px;">
+		<h2>${data[i].teams.visitors.name} @ ${data[i].teams.home.name}</h2>
+		<img src="${data[i].teams.home.logo}" style="height: 100px;">
+		<h2>${data[i].scores.visitors.points}</h2>
+		<h3>VS</h3>
+		<h2>${data[i].scores.home.points}</h2>
+		</div>`
+		games.id = ""
+		newSection = document.createElement('section')
+		newSection.setAttribute("id", "content")
+		main.appendChild(newSection)
+		games = newSection
+	}
+})
+.catch(err => {
+	console.log(err);
+});
 
-nbaEl.onsubmit = (e) => {
-	e.preventDefault();
-	let teamId = teamSelect.value;
-	location.href = `teams.html?id=${teamId}`;
-};
