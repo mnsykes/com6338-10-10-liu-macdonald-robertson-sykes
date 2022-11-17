de88cd15f89b0a8b985c9ee5b5d713283c72ce0b
Matthew Sykes <61014119+mnsykes@users.noreply.github.com>
2022-11-16 19:54:53 -0500

    homepage cleanup
    Deleted img folder and moved images into image folder. Updated src in img elements

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,12 @@
 let nbaEl = document.querySelector("#nba");
 let teamSelect = document.querySelector(".teamSelect");
 
-let games = document.querySelector("#content")
-let main = document.querySelector(".main__wrapper")
-let LocalDate = new Date()
-let Year = LocalDate.getFullYear()
-let Month = LocalDate.getMonth() + 1
-let Day = LocalDate.getDate()
-
+let games = document.querySelector("#content");
+let main = document.querySelector(".main__wrapper");
+let LocalDate = new Date();
+let Year = LocalDate.getFullYear();
+let Month = LocalDate.getMonth() + 1;
+let Day = LocalDate.getDate();
 
 const options = {
 	method: "GET",
@@ -18,14 +17,13 @@ const options = {
 };
 
 fetch(`https://api-nba-v1.p.rapidapi.com/games?date=${Year}-${Month}-${Day}`, options)
-.then((response) => response.json())
-.then((game) => {
-	console.log(`https://api-nba-v1.p.rapidapi.com/games?date=${Year}-${Month}-${Day}`)
-	console.log(game.response);
-	let data = game.response;
-	for (let i = 0; i < data.length; i++) {
-		games.innerHTML = 
-		`<div style="display: grid; grid-template-columns:25% 20% 40% 20%; grid-template-rows:50% 50%;">
+	.then((response) => response.json())
+	.then((game) => {
+		console.log(`https://api-nba-v1.p.rapidapi.com/games?date=${Year}-${Month}-${Day}`);
+		console.log(game.response);
+		let data = game.response;
+		for (let i = 0; i < data.length; i++) {
+			games.innerHTML = `<div style="display: grid; grid-template-columns:25% 20% 40% 20%; grid-template-rows:50% 50%;">
 		<h3>${Month}/${Day}</h3>
 		<img src="${data[i].teams.visitors.logo}" style="height: 100px;">
 		<h2>${data[i].teams.visitors.name} @ ${data[i].teams.home.name}</h2>
@@ -33,17 +31,17 @@ fetch(`https://api-nba-v1.p.rapidapi.com/games?date=${Year}-${Month}-${Day}`, op
 		<h2>${data[i].scores.visitors.points}</h2>
 		<h3>VS</h3>
 		<h2>${data[i].scores.home.points}</h2>
-		</div>`
-		games.id = ""
-		newSection = document.createElement('section')
-		newSection.setAttribute("id", "content")
-		main.appendChild(newSection)
-		games = newSection
-	}
-})
-.catch(err => {
-	console.log(err);
-});
+		</div>`;
+			games.id = "";
+			newSection = document.createElement("section");
+			newSection.setAttribute("id", "content");
+			main.appendChild(newSection);
+			games = newSection;
+		}
+	})
+	.catch((err) => {
+		console.log(err);
+	});
 
 fetch("https://api-nba-v1.p.rapidapi.com/teams", options)
 	.then((response) => response.json())
@@ -64,15 +62,6 @@ nbaEl.onsubmit = (e) => {
 	e.preventDefault();
 	let teamId = teamSelect.value;
 	location.href = `teams.html?id=${teamId}`;
-};
-
-
-const options = {
-	method: "GET",
-	headers: {
-		"X-RapidAPI-Key": "c204799958msh8bd639d820cabb0p1137f0jsn255151fddc1c",
-		"X-RapidAPI-Host": "api-nba-v1.p.rapidapi.com"
-	}
 };
 
 fetch("https://api-nba-v1.p.rapidapi.com/teams", options)
